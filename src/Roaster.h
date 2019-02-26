beb2f3f1d962ad1ff6d79b00b4622869436391c9
Vincent Politzer <vjapolitzer@berkeley.edu>
2019-02-25 18:16:29 -0800

    Create Roaster.cpp

--- src/Roaster.h
+++ src/Roaster.h
@@ -8,19 +8,24 @@
 #include "PID_lib.h"
 #include "Triac_lib.h"
 
-#define TC_PIN_1 7
-#define TC_PIN_2 6
-
+// Placeholder pin definitions, subject to change
 #define DISP_CS 10
 #define DISP_DC 9
 #define DISP_RST 8
+
+#define TC_PIN_1 7
+#define TC_PIN_2 6
+
+#define HEAT_PIN 5
+#define FAN_PIN 4
+
+#define TC_UPDATE_PERIOD 300
 
 enum Mode {Menu, Roasting, Cooling};
 
 class Roaster
 {
     public:
-        Roaster();
         void begin();
         void input(uint8_t);
         bool update();
@@ -32,13 +37,14 @@ class Roaster
 
         static Mode mode;
         static uint8_t menuPage;
-        static U8G2_SH1106_128X64_NONAME_F_4W_HW_SPI roasterDisp;
+        static U8G2_SH1106_128X64_NONAME_F_4W_HW_SPI oled;
         static MAX6675 tc1, tc2;
         static Triac heater, fan;
         static PID roastPID;
 
         static double inputPID, outputPID, setpointPID;
         static double tcTemp1, tcTemp2, tcTempAvg;
+        static double pGain, iGain, dGain;
 };
 
 #endif
